131dd5f0d361a3772cb4a5c3255a0528c433b9df
Anderson0017 <andeerson0017@gmail.com>
2020-06-11 19:54:45 -0300

    configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'ola' })
-})
+app.get('/', (request, response) => response.json({ message: 'ola' }));
 
 app.listen(3333, () => {
-  console.log('dgdfgdgdfg')
+  console.log('dgdfgdgdfg');
 });

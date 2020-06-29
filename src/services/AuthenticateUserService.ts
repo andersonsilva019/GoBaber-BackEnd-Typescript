ec26a6ded1d93cd115d035f72abcf4032967f76d
Anderson0017 <andeerson0017@gmail.com>
2020-06-29 16:30:54 -0300

    Gerando token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'e74d52e5c15d7d308213fd6f45788737', {
+      subject: user.id,
+      expiresIn: '7d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }

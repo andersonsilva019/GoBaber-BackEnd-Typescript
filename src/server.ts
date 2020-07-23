6066b6578bdd46011670cbfba9b83c2a06641450
Anderson0017 <andeerson0017@gmail.com>
2020-07-23 19:02:24 -0300

    habilitando cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import cors from 'cors';
 import './database';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);

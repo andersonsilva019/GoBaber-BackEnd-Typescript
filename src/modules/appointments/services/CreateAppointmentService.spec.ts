7b315c2ab50a3a91fbb73ee04f6277990171eb4f
Anderson Silva <andeerson0017@gmail.com>
2020-11-07 19:41:09 -0300

    :white_check_mark: test: nao permitir dois agendamentos no mesmo horario

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakerAppointmentsRepository from '../repositories/fakes/FakerAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('12354848484');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakerAppointmentsRepository = new FakerAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakerAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11); // 10/04/2020 11:00h
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12354848484',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12354848484',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
